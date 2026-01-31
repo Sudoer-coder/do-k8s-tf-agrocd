eb6ec77e65c7e56d730c84fe4652247c56168dc9
SUDO <sudo-coder@users.noreply.github.com>
2026-01-31 14:45:59 +0700

    Updated Cluster & Variables 2nd

--- terraform/nodepool.tf
+++ terraform/nodepool.tf
@@ -1,15 +1,22 @@
 resource "digitalocean_kubernetes_node_pool" "medium" {
   cluster_id = digitalocean_kubernetes_cluster.this.id
 
-  name = "workload-pool"
+  name = "medium-pool"
   size = var.nodepool_size
 
   auto_scale = true
-  min_nodes  = var.nodepool_min_nodes
-  max_nodes  = var.nodepool_max_nodes
+  min_nodes  = var.mid_nodepool_min_nodes
+  max_nodes  = var.mid_nodepool_max_nodes
+
+  labels = {
+    pool  = "medium"
+    phase = "scale"
+    role  = "workload"
+  }
 
   tags = [
-    "env:${var.environment}",
-    "cluster:${var.cluster_name}"
+    "pool:medium",
+    "phase:scale",
+    "env:${var.environment}"
   ]
 }
